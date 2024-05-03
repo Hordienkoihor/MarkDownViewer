1c415f630462dc886c75384c6659be19989636e2
unknown <garikgord05@gmail.com>
2024-05-03 20:45:24 +0300

    tab

--- public/script.js
+++ public/script.js
@@ -3,6 +3,24 @@ window.onload = function() {
     let pad = document.getElementById('pad');
     let markdownArea = document.getElementById('markdown');
 
+    pad.addEventListener('keydown', function(e){
+        if(e.keyCode === 9){
+            let start = e.selectionStart;
+            let end = e.selectionEnd;
+
+            let target = e.target;
+            let value = target.value;
+
+            target.value = value.substring(0, start) + "\t" + value.substring(end);
+
+            this.selectionStart = this.selectionEnd = start +1
+
+            e.preventDefault()
+        }
+    })
+
+    let previousMarkdownValue;
+
     let convertTextAreaToMarkdown = function() {
         let markdownText = pad.value
         previousMarkdownValue = markdownText
@@ -25,8 +43,14 @@ window.onload = function() {
 
     pad.addEventListener('input', convertTextAreaToMarkdown);
 
-    sharejs.open(document.location.pathname, 'text', (error, doc) => {
-        doc.attach_textarea(pad)
-        convertTextAreaToMarkdown()
-    })
+    if(document.location.pathname.length > 1){
+
+        var documentName = document.location.pathname.substring(1);
+        sharejs.open(documentName, 'text', function(error, doc) {
+            doc.attach_textarea(pad);
+            convertTextAreaToMarkdown();
+        });
+    }
+
+    convertTextAreaToMarkdown();
 }
